bc6d53b95e4bc1e2c398ca005717164a091f4eef
AndruHard <andru.volov2001@gmail.com>
2022-04-28 19:45:38 +0300

    router P1

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new \ishop\App();
 
 //debug(\ishop\App::$app->getProperties());
-throw new Exception('Page not found');
+
+//debug(\ishop\Router::getRoutes());
