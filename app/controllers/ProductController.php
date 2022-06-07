57f159450e3f05d3e9424b2ee9e4d5616edc4c89
AndruHard <andru.volov2001@gmail.com>
2022-06-07 19:33:49 +0300

    m product

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -4,6 +4,7 @@
 namespace app\controllers;
 
 
+
 class ProductController extends AppController
 {
     public function viewAction(){
@@ -12,10 +13,17 @@ class ProductController extends AppController
         if(!$product){
             throw new \ErrorException('Page Not found', 404);
         }
+
+        $related = \R::getAll("SELECT * FROM related_product JOIN product ON
+product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
+
+
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product'));
+        $this->set(compact('product', 'related'));
 
     }
+
+
 
 
 
