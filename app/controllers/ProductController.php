f6a935d674a2a7cce5e094d436473149b4bdbf0c
AndruHard <andru.volov2001@gmail.com>
2022-06-12 14:21:29 +0300

    card Product

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -5,6 +5,9 @@ namespace app\controllers;
 
 
 
+use app\models\Breadcrumbs;
+use app\models\Product;
+
 class ProductController extends AppController
 {
     public function viewAction(){
@@ -14,12 +17,29 @@ class ProductController extends AppController
             throw new \ErrorException('Page Not found', 404);
         }
 
+        $breadcrumbs = Breadcrumbs::getBreadcrumbs($product->category_id, $product->title);
+
+        //связанные товары
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON
 product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
 
+//        запись в куки
+        $p_model = new Product();
+        $p_model->setRecentlyViewed($product->id);
+
+//        просмотренные товары
+        $r_viewed = $p_model->getRecentlyViewed();
+        $recentlyViewed = null;
+        if($r_viewed){
+            $recentlyViewed = \R::find('product', 'id IN (' . \R::genSlots($r_viewed) . ') LIMIT 3', $r_viewed);
+        }
+
+        //галерея
+        $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
+
 
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related'));
+        $this->set(compact('product', 'related', 'gallery', 'recentlyViewed', 'breadcrumbs'));
 
     }
 
